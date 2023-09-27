adaa08a6bebe3f2afb8d53fba262b8b042599000
Faith Muraya <111008831+faith-muraya@users.noreply.github.com>
2023-09-27 15:23:09 +0300

    final commit

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-bookingElement = document.getElementById('booking');
+const bookingElement = document.getElementById('booking');
 
 bookingElement.addEventListener("click", () => {
     alert('Thankyou for booking us')
 })
+
+const names = document.querySelectorAll('.me');
+
+names.forEach(name => {
+    const originalText = name.textContent;
+
+    name.addEventListener('mouseover', () => {
+        name.textContent = `Hello My Name is ${originalText}`;
+    });
+
+    name.addEventListener('mouseout', () => {
+        name.textContent = originalText;
+    });
+});
+
+
